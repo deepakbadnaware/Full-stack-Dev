1bfd34bc99256e7ff3129fca244b070dba162402
deepakbadnaware <deepakbadnaware69@gmail.com>
2026-05-06 10:44:52 +0530

    Solve LeetCode questions and Practice

--- ProgrammingLanguage/Star.py
+++ ProgrammingLanguage/Star.py
@@ -1,4 +1,54 @@
-for i in range(1500,2701):
-    if (i%7==0) and (i%5==0):
-        print(i)
-    
+# # for i in range(1500,2701):
+# #     if (i%7==0) and (i%5==0):
+# #         print(i)
+
+# """
+# Star Pattern   
+# for i in range(5):
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+# for i in range(6,0,-1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+# Answer
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+# * * * * * * 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# * 
+# """
+
+# def ReverseWord(str):
+#     return str[::-1]
+
+# print(ReverseWord("Deepak"))
+
+
+# arr=[8,1,2,2,3]
+
+# n=len(arr)
+# newArr=[]
+# for i in arr:
+#     count=0
+#     for j in arr:
+#         if j<i:
+#             count+=1
+#     newArr.append(count)
+
+# print(newArr)
+
+
+# nums=[0,0,1,1,1,2,2,3,3,4]
+# # for i in range(1,len(nums)):
+# #     print (nums[i])
+
+# val=3
+
